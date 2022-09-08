932d68abca619d0380dd3ec2c403ae719fc0f4b1
kkk <you@example.com>
2022-09-08 15:21:51 +0800

    第六次

--- 服装/1.js
+++ 服装/1.js
@@ -1,60 +1,87 @@
-var span = document.querySelector('.nav .moon')
+var span = document.querySelectorAll('.nav .moon')
 var nav = document.querySelector('.nav');
 console.log(span);
 var bool = true
-var arr=['b1','b4','b3']
-var item=['your new style','custom made suits','styles and trends'];
-var bg=document.querySelector('.bg');
- var num=0
- var next = document.querySelector('.bg .next');
- var before = document.querySelector('.bg .before');
-next.onclick=show;
-before.onclick=function(){
-    if (num<=0) {
-        num=arr.length-1
+var arr = ['b1', 'b4', 'b3']
+var item = ['your new style', 'custom made suits', 'styles and trends'];
+var bg = document.querySelector('.bg');
+var num = 0
+var next = document.querySelector('.bg .next');
+var before = document.querySelector('.bg .before');
+var list = document.querySelector(".i i");
+var vs= document.querySelector('.nav .visible-xs')
+var vs2= document.querySelector('.nav .visible-xs .nav-name')
+next.onclick = show;
+before.onclick = function () {
+    if (num <= 0) {
+        num = arr.length - 1
     }
-    document.querySelector('.bg h1').innerText=item[num];
-    bg.style.backgroundImage="url('../整容/img/"+arr[num]+".jpg')";
+    document.querySelector('.bg h1').innerText = item[num];
+    bg.style.backgroundImage = "url('../整容/img/" + arr[num] + ".jpg')";
     num--
 }
-function show(){
-    if (num>=arr.length) {
-        num=0
+function show() {
+    if (num >= arr.length) {
+        num = 0
     }
-    document.querySelector('.bg h1').innerText=item[num];
-    bg.style.backgroundImage="url('../整容/img/"+arr[num]+".jpg')";
+    document.querySelector('.bg h1').innerText = item[num];
+    bg.style.backgroundImage = "url('../整容/img/" + arr[num] + ".jpg')";
     num++
 }
 
-setInterval(show,4000)
+setInterval(show, 4000)
 window.onscroll = function () {
     console.log(scrollY);
     if (scrollY !== 0) {
-        nav.style.backgroundColor = "white";
+        nav.style.backgroundColor = "lightyellow";
         nav.style.color = "black";
-        span.style.color = "black";
+        // span.style.color="black"
+        vs2.style.backgroundColor="white";
+        span[0].style.color = "red"; 
         document.querySelectorAll('.nav a').forEach(function (e) {
             e.style.color = "black";
         })
     } else {
         nav.style.backgroundColor = "";
         nav.style.color = "white";
-        span.style.color = "white";
+        vs2.style.backgroundColor="black";
+        span[0].style.color = "white";
         document.querySelectorAll('.nav a').forEach(function (e) {
             e.style.color = "white";
         })
     }
-    span.onclick = function () {
-        console.log(span);
-        if (bool) {
-            document.querySelector('.nav span i').className = "fa fa-sun-o";
-            bool = false
-        }
-        else {
-            document.querySelector('.nav span i').className = "fa fa-moon-o";
-            bool = true
-        }
-        document.body.classList.toggle("act");
-        nav.classList.toggle('act')
+}
+span.forEach(function(e){
+
+e.onclick = function () {
+    console.log(e.children);
+    if (bool) {
+        e.firstChild.className = "fa fa-sun-o";
+        bool = false
+    }
+    else {
+        e.firstChild.className = "fa fa-moon-o";
+        bool = true
+    }
+    document.body.classList.toggle("act");
+    nav.classList.toggle('act')
+}})
+var bool1 = true
+
+console.log(vs);
+list.onclick = function () {
+    // document.querySelector('.nav .row').classList.toggle('add')
+    if (bool1) {
+        document.querySelector('.nav .visible-xs').classList.add('add')
+        document.querySelector('.nav .visible-xs').classList.remove('remover')
+        bool1 = false
+        list.className = "fa fa icon-close fa-times";
+    }
+    else {
+        document.querySelector('.nav .visible-xs').classList.remove('add')
+        document.querySelector('.nav .visible-xs').classList.add('remover')
+        // document.querySelector('.nav .row').classList.add('add')
+        list.className = "fa icon-expand fa-bars";
+        bool1 = true
     }
 }
